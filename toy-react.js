540d3215945b23a1f94b923c0ca3e53e33ef2f4a
unknown <wangxuanniu@yeah.net>
2020-09-26 11:17:31 +0800

    ignore

--- toy-react.js
+++ toy-react.js
@@ -1,3 +1,5 @@
+const RENDER_TO_DOM = Symbol('render to dom')
+
 class ElementWrapper {
     constructor(type) {
         this.root = document.createElement(type)
@@ -8,11 +10,17 @@ class ElementWrapper {
     appendChild(component) {
         this.root.appendChild(component.root)
     }
+    [RENDER_TO_DOM](range) {
+        this.render().[RENDER_TO_DOM](range)
+    }
 }
 
 class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content)
+    }
+    [RENDER_TO_DOM](range) {
+        this.render().[RENDER_TO_DOM](range)
     }
 }
 export class Component {
@@ -27,11 +35,8 @@ export class Component {
         console.log(component, 999)
         this.children.push(component)
     }
-    get root() {
-        if(!this._root) {
-            this._root = this.render().root
-        }
-        return this._root
+    [RENDER_TO_DOM](range) {
+        this.render()[RENDER_TO_DOM](range)
     }
 }
 
@@ -64,5 +69,10 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function render(component, parentElement) {
-    parentElement.appendChild(component.root)
+    let range = document.createRange()
+    range.setStart(parentElement, 0)
+    range.setEnd(parentElement, parentElement.childNodes.length)
+    range.deleteContents()
+    component.render()[RENDER_TO_DOM](range)
+
 }
